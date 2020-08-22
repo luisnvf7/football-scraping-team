ac303cf89d0dae9ceee9c0a0f4a46f47965ee460
Gustavo Parra <gustavoparrabotero@gmail.com>
2020-08-22 00:52:53 -0400

    Adding the whole code to Gustavo's branch

--- Main.py
+++ Main.py
@@ -2,9 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 
 headers = {'User-Agent':
-           'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+               'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
 
-page = "https://www.transfermarkt.co.uk/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2000"
-pageTree = requests.get(page, headers=headers)
-pageSoup = BeautifulSoup(pageTree.content, 'html.parser')
+url = "https://www.transfermarkt.es/paris-saint-germain-fc/kader/verein/583/saison_id/2019/plus/1"
+source = requests.get(url, headers=headers)
+soup = BeautifulSoup(source.content, 'html.parser')
+
+table = soup.find('table', class_="items")
+
+tr = table.find('tr')
+
+players_info = {}
+
+for th in tr.find_all("th")[1:]:
+    players_info[th.text] = ""
+
+tbody = table.find("tbody")
+
+tbody_tr = tbody.find_all("tr")
+
+array = []
+
+for tr in tbody_tr:
+    for td in tr.find_all("td", class_=True)[1:]:
+        if td["class"] == ['posrela']:
+            table_td = td.find('table', class_="inline-table")
+            for tbody in table_td.find_all('tr'):
+
 
