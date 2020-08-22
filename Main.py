75d6ef3d7e09003af4e18f36f4bc44fbeb18191a
Gustavo Parra <gustavoparrabotero@gmail.com>
2020-08-22 01:15:09 -0400

    Going back

--- Main.py
+++ Main.py
@@ -8,25 +8,8 @@ url = "https://www.transfermarkt.es/paris-saint-germain-fc/kader/verein/583/sais
 source = requests.get(url, headers=headers)
 soup = BeautifulSoup(source.content, 'html.parser')
 
-table = soup.find('table', class_="items")
+table = soup.find('table', class_='items')
 
-tr = table.find('tr')
 
-players_info = {}
-
-for th in tr.find_all("th")[1:]:
-    players_info[th.text] = ""
-
-tbody = table.find("tbody")
-
-tbody_tr = tbody.find_all("tr")
-
-array = []
-
-for tr in tbody_tr:
-    for td in tr.find_all("td", class_=True)[1:]:
-        if td["class"] == ['posrela']:
-            table_td = td.find('table', class_="inline-table")
-            for tbody in table_td.find_all('tr'):
             
 
