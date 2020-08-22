9ea9ccbb700e672486d25d035b3016eeac8a99e4
Gustavo Parra <gustavoparrabotero@gmail.com>
2020-08-21 22:47:52 -0400

    Adding headers

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
-import
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent':
+           'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+
+page = "https://www.transfermarkt.co.uk/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2000"
+pageTree = requests.get(page, headers=headers)
+pageSoup = BeautifulSoup(pageTree.content, 'html.parser')
+
