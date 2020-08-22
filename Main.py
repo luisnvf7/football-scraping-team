593386b0b007a865a1e549f8bd4f1e164cc46f43
Gustavo Parra <gustavoparrabotero@gmail.com>
2020-08-22 14:57:15 -0400

    Commit

--- Main.py
+++ Main.py
@@ -15,3 +15,11 @@ tbody_table = table.find('tbody')
 for td in tbody_table.find_all('td'):
     if td['class'] == ['hauptlink']:
         print(td.text)
+
+for tr in tbody_tr:
+    for td in range(1, len(tr.find_all("td"))):
+        if td == 1:
+            for td_name in tr.find_all("td")[td].find_all("td", class_="hauptlink"):
+                nombres.append(td_name.find("a").get_text())
+
+print(nombres)
